b9623b6c22c23ffb5f593d4ac899acde864a6865
Jeso <15325061080104@cetis108.edu.mx>
2017-06-02 22:16:08 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/cetis108/cetis108app004/MapsActivity.java
@@ -0,0 +1,48 @@
+package mx.edu.cetis108.cetis108app004;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(25.585919,-108.463950 );
+        mMap.addMarker(new MarkerOptions().position(sydney).title(" 15325061080104 Villanueva Rubio Jesús Emmanuel"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
